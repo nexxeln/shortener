cf944e10ca18a4165897ec7cb2c44d0cad7b6f56
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-08 15:46:26 +0530

    control input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 import { Button, Container, TextInput, Title } from "@mantine/core";
 
 const Home: NextPage = () => {
+  const [value, setValue] = useState("https://nexxel.dev");
+
   return (
     <>
       <Container>
         <Title mb="xl">SHORTENER</Title>
         <form>
-          <TextInput label="URL" required />
+          <TextInput
+            label="URL"
+            required
+            value={value}
+            onChange={(event) => setValue(event.currentTarget.value)}
+          />
           <Button type="submit" my={10} size="sm" color="violet">
             Shorten
           </Button>
