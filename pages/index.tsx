711ec3fe696be2f79a9bc686e94bf12f79548e21
Shoubhit Dash <shoubhit2005@gmail.com>
2022-05-08 15:49:43 +0530

    create onSubmit function

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,21 @@ import { Button, Container, TextInput, Title } from "@mantine/core";
 const Home: NextPage = () => {
   const [value, setValue] = useState("https://nexxel.dev");
 
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await fetch("/api/shorten", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ url: value }),
+    });
+  };
+
   return (
     <>
       <Container>
         <Title mb="xl">SHORTENER</Title>
-        <form>
+        <form onSubmit={onSubmit}>
           <TextInput
             label="URL"
             required
